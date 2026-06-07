33580ed5070ba0138f612d12e7f63988a3f74c80
pbrusco <pablo.brusco@gmail.com>
2026-06-07 13:24:27 -0400

    Display app version v1.1.0 in footer and bump cache version to atomic-v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "atomic-v7";
+const CACHE = "atomic-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-maskable.svg"];
 
 self.addEventListener("install", (e) => {
